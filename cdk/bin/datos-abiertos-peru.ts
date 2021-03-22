03c780b5ec5e427a70f375a86f984bc9b24656b2
William Perez <william.perez@pucp.edu.pe>
2021-03-22 00:26:55 -0500

    - CDK stack update: S3 bucket, DynamoDB table, Fetch function, Event trigger.

--- cdk/bin/datos-abiertos-peru.ts
+++ cdk/bin/datos-abiertos-peru.ts
@@ -3,5 +3,16 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { DatosAbiertosPeruStack } from '../lib/datos-abiertos-peru-stack';
 
+const AWS_REGION = 'us-east-2'
+const PROJECT_TAG = 'datos-abiertos-peru'
+
 const app = new cdk.App();
-new DatosAbiertosPeruStack(app, 'DatosAbiertosPeruStack', { env: { region: 'us-east-1' }, tags: { project: 'datos-abiertos' } });
+
+new DatosAbiertosPeruStack(app, 'DatosAbiertosPeruStack', {
+  env: {
+    region: AWS_REGION
+  },
+  tags: {
+    project: PROJECT_TAG
+  }
+});
--- cdk/lib/datos-abiertos-peru-stack.ts
+++ cdk/lib/datos-abiertos-peru-stack.ts
@@ -1,10 +1,52 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3'
+import * as s3 from '@aws-cdk/aws-s3';
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as lambda from '@aws-cdk/aws-lambda'
+import { Rule, Schedule } from '@aws-cdk/aws-events'
+import { LambdaFunction } from '@aws-cdk/aws-events-targets'
+import { Duration } from '@aws-cdk/core';
 
 export class DatosAbiertosPeruStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
-    new s3.Bucket(this, 'datos-abiertos-cdk-test', {versioned: false})
+    const dataBucket = new s3.Bucket(this, 'dap-data', {
+      versioned: false
+    })
+
+    const hashesTable = new dynamodb.Table(this, 'dap_md5_hashes', {
+      partitionKey: {
+        name: 'asset_name',
+        type: dynamodb.AttributeType.STRING
+      },
+      readCapacity: 1,
+      writeCapacity: 1,
+    })
+
+    const fetchFunction = new lambda.Function(this, 'dap_fn_fetch', {
+      handler: 'minsa_vacunacion.get_dataset',
+      runtime: lambda.Runtime.PYTHON_3_8,
+      code: lambda.Code.fromAsset('../src/fetch'),
+      memorySize: 512,
+      timeout: Duration.minutes(5),
+      environment: {
+        "DDB_HASHES_TABLE": hashesTable.tableName,
+        "S3_DATA_BUCKET": dataBucket.bucketName
+      }
+    })
+
+    hashesTable.grantReadWriteData(fetchFunction)
+    dataBucket.grantWrite(fetchFunction)
+
+    const fetchRequestLayer = lambda.LayerVersion
+      .fromLayerVersionArn(this, 'dap_layer_requests', 'arn:aws:lambda:us-east-2:770693421928:layer:Klayers-python38-requests-html:37')
+    
+    fetchFunction.addLayers(fetchRequestLayer)
+
+    const fnTarget = new LambdaFunction(fetchFunction)
+    const fetchTrigger = new Rule(this, 'dap_trigger_event', {
+      schedule: Schedule.cron({ minute: '24', hour: '5' }),
+      targets: [fnTarget]
+    })
   }
 }
