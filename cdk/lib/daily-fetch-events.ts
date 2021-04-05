d4954c920755315c2bdfa62a13de774dc64fc3cb
William Perez <william.perez@pucp.edu.pe>
2021-04-04 23:52:34 -0500

    Compile into single catalog and create events only for cron scheduled datasets

--- cdk/lib/daily-fetch-events.ts
+++ cdk/lib/daily-fetch-events.ts
@@ -4,17 +4,18 @@ import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 import { Rule, RuleTargetInput, Schedule } from "@aws-cdk/aws-events";
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
+import { print } from "util";
 
 
 interface SourceDescription {
   Name: string,
   URI: string,
   Filename: string,
-  CronExpression: string
+  CronExpression?: string
 }
 
 function readSourcesCatalog(): object {
-  let fileContents = fs.readFileSync('../src/catalogs/daily.yml', 'utf-8');
+  let fileContents = fs.readFileSync('../src/fetch/catalog.yml', 'utf-8');
   let sourcesCatalogYaml = yaml.load(fileContents);
   return typeof sourcesCatalogYaml == 'object' ? sourcesCatalogYaml : Object()
 }
@@ -27,18 +28,20 @@ export class DAPScheduledFetchEvents extends Construct {
 
     for (let [_, item] of Object.entries(sourcesCatalog)) {
       let itemDescription = item as SourceDescription;
-      let customPayload = {
-        asset_url: itemDescription.URI,
-        asset_filename: itemDescription.Filename
-      };
-      let eventTarget = new LambdaFunction(targetFn, {
-        event: RuleTargetInput.fromObject(customPayload)
-      });
-
-      new Rule(this, `trigger_${itemDescription.Name}`, {
-        schedule: Schedule.expression(`cron(${itemDescription.CronExpression})`),
-        targets: [eventTarget]
-      });
+      if (itemDescription.hasOwnProperty('CronExpression')) {
+        let customPayload = {
+          asset_url: itemDescription.URI,
+          asset_filename: itemDescription.Filename
+        };
+        let eventTarget = new LambdaFunction(targetFn, {
+          event: RuleTargetInput.fromObject(customPayload)
+        });
+  
+        new Rule(this, `trigger_${itemDescription.Name}`, {
+          schedule: Schedule.expression(`cron(${itemDescription.CronExpression})`),
+          targets: [eventTarget]
+        });
+      }
     }
   }
 }
