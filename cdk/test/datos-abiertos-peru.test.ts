35e976f9fc06d645b8eb4973a83c77e343e7e53d
William Perez <william.perez@pucp.edu.pe>
2021-04-10 15:03:08 -0500

    comment test code

--- cdk/test/datos-abiertos-peru.test.ts
+++ cdk/test/datos-abiertos-peru.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as DatosAbiertosPeru from '../lib/datos-abiertos-peru-stack';
+// import * as DatosAbiertosPeru from '../lib/datos-abiertos-peru-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new DatosAbiertosPeru.DatosAbiertosPeruStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+// test('Empty Stack', () => {
+//     const app = new cdk.App();
+//     // WHEN
+//     const stack = new DatosAbiertosPeru.DatosAbiertosPeruStack(app, 'MyTestStack');
+//     // THEN
+//     expectCDK(stack).to(matchTemplate({
+//       "Resources": {}
+//     }, MatchStyle.EXACT))
+// });
