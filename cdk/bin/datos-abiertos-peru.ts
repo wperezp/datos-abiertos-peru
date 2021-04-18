5a30027d7e949ce1bb0cbdfed37e9a60b5aa8cd1
William Perez <william.perez@pucp.edu.pe>
2021-04-17 23:09:12 -0500

    Moving staging function to base stack

--- cdk/bin/datos-abiertos-peru.ts
+++ cdk/bin/datos-abiertos-peru.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import * as s3n from "@aws-cdk/aws-s3-notifications";
 import { DAPBaseStack } from '../lib/base-stack';
 import { DAPFetchStack } from '../lib/fetch-stack';
 import { DAPScheduledFetchEventsStack } from '../lib/fetch-events-stack';
-import { DAPStagingStack } from '../lib/staging-stack'
+
 
 const AWS_REGION = process.env.AWS_DEFAULT_REGION;
 const PROJECT_TAG = 'datos-abiertos-peru';
@@ -15,4 +16,3 @@ const props = {tags: {project: PROJECT_TAG}, env: {region: AWS_REGION}}
 const baseStack = new DAPBaseStack(app, 'DAPBaseStack', props);
 const fetchStack = new DAPFetchStack(app, 'DAPFetchStack', baseStack.vpc, baseStack.sourceDataBucket, baseStack.hashesTable, props);
 const eventsStack = new DAPScheduledFetchEventsStack(app, 'DAPScheduledFetchEventsStack', fetchStack.fetchFn, props);
-const stagingStack = new DAPStagingStack(app, 'DAPStagingStack', baseStack.sourceDataBucket, props);
--- cdk/lib/base-stack.ts
+++ cdk/lib/base-stack.ts
@@ -1,7 +1,9 @@
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as ec2 from "@aws-cdk/aws-ec2";
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Construct, Duration, Stack, StackProps } from "@aws-cdk/core";
+import { LambdaDestination } from "@aws-cdk/aws-s3-notifications";
 
 export class DAPBaseStack extends Stack {
   
@@ -9,6 +11,7 @@ export class DAPBaseStack extends Stack {
   readonly provisioningDataBucket: s3.Bucket;
   readonly hashesTable: dynamodb.Table;
   readonly vpc: ec2.Vpc;
+  readonly fnStaging: lambda.Function;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -39,5 +42,18 @@ export class DAPBaseStack extends Stack {
         },
       ],
     });
+
+    // Including also staging function due to event dependency
+    this.fnStaging = new lambda.DockerImageFunction(this, "fnCleaning",{
+      code: lambda.DockerImageCode.fromImageAsset('../src/staging/'),
+      timeout: Duration.minutes(15),
+      environment: {
+        S3_SOURCE_BUCKET: this.sourceDataBucket.bucketName
+      }
+    });
+    
+    this.sourceDataBucket.grantReadWrite(this.fnStaging);
+    this.sourceDataBucket.addObjectCreatedNotification(new LambdaDestination(this.fnStaging), {prefix: 'raw/'});
+    
   }
 }
