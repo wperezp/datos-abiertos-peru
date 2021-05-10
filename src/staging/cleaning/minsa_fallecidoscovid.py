f6a508c1156aaa41f6b1e203298fb90cc2e1d1cc
William Perez <william.perez@pucp.edu.pe>
2021-05-09 21:07:26 -0500

    fix io

--- src/staging/cleaning/minsa_fallecidoscovid.py
+++ src/staging/cleaning/minsa_fallecidoscovid.py
@@ -3,8 +3,9 @@ import io
 import pandas as pd
 
 
-def clean(data: io.BytesIO) -> pd.DataFrame:
-    df = pd.read_csv(data, sep=';', header=0)
+def clean(data: bytes) -> pd.DataFrame:
+    data_io = io.BytesIO(data)
+    df = pd.read_csv(data_io, sep=';', header=0)
     df['FECHA_CORTE'] = pd.to_datetime(df['FECHA_CORTE'], format='%Y%m%d')
     df['FECHA_FALLECIMIENTO'] = pd.to_datetime(df['FECHA_FALLECIMIENTO'], format='%Y%m%d')
     df['FECHA_NAC'] = pd.to_datetime(df['FECHA_NAC'], format='%Y%m%d')
