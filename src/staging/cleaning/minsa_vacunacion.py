61e48288952cb496d906f9bd5c675811d495ec6a
William Perez <william.perez@pucp.edu.pe>
2021-09-05 20:53:56 -0500

    now it's semicolon and different date format

--- src/staging/cleaning/minsa_vacunacion.py
+++ src/staging/cleaning/minsa_vacunacion.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def clean(data: bytes) -> pd.DataFrame:
     data_io = io.BytesIO(data)
-    df = pd.read_csv(data_io, sep=',', dtype=str)
+    df = pd.read_csv(data_io, sep=';', dtype=str)
     # df = df.fillna('NULL')
-    df['FECHA_CORTE'] = pd.to_datetime(df['FECHA_CORTE'], format='%Y%m%d')
-    df['FECHA_VACUNACION'] = pd.to_datetime(df['FECHA_VACUNACION'], format='%Y%m%d')
+    df['FECHA_CORTE'] = pd.to_datetime(df['FECHA_CORTE'], format='%Y-%m-%d')
+    df['FECHA_VACUNACION'] = pd.to_datetime(df['FECHA_VACUNACION'], format='%Y-%m-%d')
     return df
